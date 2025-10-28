5eb6bb9053921e0fc54d14948468702a977ac813
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-10-28 12:20:11 +0530

    feat: require enum variants to be dataclasses
    prevent user from declaring extra functions on individual variants

--- src/ape/__init__.py
+++ src/ape/__init__.py
@@ -1,10 +1,32 @@
 import inspect
+from dataclasses import is_dataclass
 
 
-def AlgebraicEnum(cls):
-    for subclass_name, subclass in inspect.getmembers(cls, predicate=inspect.isclass):
-        if subclass_name != "__class__":
-            setattr(cls, subclass_name, type(subclass_name, (cls, subclass), {}))
+def AlgebraicEnum(enum_base):
+    for variant_name, variant in inspect.getmembers(enum_base, predicate=inspect.isclass):
+        if variant_name == "__class__":
+            continue
 
-    return cls
+        if not is_dataclass(variant):
+            raise Exception(
+                f"AlgebraicEnum variants are required to be dataclasses. Consider adding the `@dataclass` decorator to variant `{variant_name}`."
+            )
 
+        extra_declared_functions = {
+            function_name
+            for function_name, _fn in inspect.getmembers(
+                variant, predicate=inspect.isfunction
+            )
+        } - {
+            "__init__",
+            "__repr__",
+            "__eq__",
+            "__replace__",
+        }
+
+        if extra_declared_functions:
+            raise Exception(
+                f"Functions must not be declared on individual enum variants, found functions declared on variant {variant_name}: {', '.join(extra_declared_functions)}."
+            )
+        setattr(enum_base, variant_name, type(variant_name, (enum_base, variant), {}))
+    return enum_base
